c35c09ccacb80a869b792df2881c9f2a5250d0ef
taku <takunagase1025@gmail.com>
2023-12-25 15:53:15 +0900

    擬似apiの読み込みコード更新（app.py）

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-stores = [
-    {
-        "name": "Store1",
-        "items": [
-            {
-                "name": "Chair",
-                "price": 15.99
-            }
-        ]
-    }
-]
-
-
 from flask import Flask
+import requests
+
 app = Flask(__name__)
 
-@app.route("/store")
-def get_stores():
-    return {"stores": stores}
+@app.route("/fetchtest")
+def fetchtest():
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
+    return response.json(), 200
 
